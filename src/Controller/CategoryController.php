adcdb20773ee485c9ecb6c5ed2e8d237f6f14c7f
mohammedsabouni <mohammed.sabouni.2002@gmail.com>
2026-04-03 01:44:39 +0100

    add crud categories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,17 +2,75 @@
 
 namespace App\Controller;
 
+use App\Dto\CategoryDto;
+use App\Entity\Category;
+use App\Form\CategoryFormType;
+use App\Mapper\CategoryMapper;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class CategoryController extends AbstractController
 {
+    public function __construct(
+        private CategoryMapper $mapper,
+        private EntityManagerInterface $em,
+    ) {}
     #[Route('/category', name: 'app_category')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
+        $categories = $categoryRepository->findAll();
+
         return $this->render('category/index.html.twig', [
             'controller_name' => 'CategoryController',
+            'categories' => $categories,
         ]);
+    }
+
+    #[Route('/category/add', name: 'app_category_add', methods: ['GET','POST'])]
+    public function add(Request $request): Response
+    {
+        $categoryDto = new CategoryDto();
+        $form = $this->createForm(CategoryFormType::class, $categoryDto);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $this->mapper->toEntity($categoryDto);
+            $this->em->persist($category);
+            $this->em->flush();
+            return $this->redirectToRoute('app_category');
+        }
+        return $this->render('category/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route("/category/{id}/edit", name: 'category_edit', methods: ['GET','POST'])]
+    public function edit(Request $request, Category $category): Response
+    {
+        $dto = $this->mapper->toDto($category);
+        $form = $this->createForm(CategoryFormType::class, $dto);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->mapper->updateEntity($category, $dto);
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_category');
+        }
+
+        return $this->render('category/edit.html.twig', [
+            'form' => $form,
+        ]);
+    }
+    #[Route('/category/{id}/delete', name: 'category_delete', methods: ['POST'])]
+    public function delete(Category $category, EntityManagerInterface $em, Request $request): Response
+    {
+        $em->remove($category);
+        $em->flush();
+
+        return $this->redirectToRoute('app_category');
     }
 }
---
+++ src/Mapper/CategoryMapper.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mapper;
+
+use App\Dto\CategoryDto;
+use App\Entity\Category;
+
+class CategoryMapper
+{
+    public function toEntity(CategoryDto $dto): Category
+    {
+        $category = new Category();
+        $category->setName($dto->getName());
+        $category->setDescription($dto->getDescription());
+
+        return $category;
+    }
+
+    public function toDto(Category $category): CategoryDto
+    {
+        $dto = new CategoryDto();
+        $dto->setName($category->getName());
+        $dto->setDescription($category->getDescription());
+
+        return $dto;
+    }
+
+    public function updateEntity(Category $category, $dto)
+    {
+        $category->setName($dto->getName());
+        $category->setDescription($dto->getDescription());
+    }
+}
